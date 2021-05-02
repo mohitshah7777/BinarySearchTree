4c71be5e63e5d934e28d49f4151a367b1daff500
Mohit Shah <mohitshah7777@gmail.com>
2021-05-02 21:20:56 +0530

    Added: To create Binary search tree and add elemets to it

--- src/main/java/com/bridgelabz/bst/MyBinarySearchTree.java
+++ src/main/java/com/bridgelabz/bst/MyBinarySearchTree.java
@@ -1,4 +1,33 @@
 package com.bridgelabz.bst;
 
-public class MyBinarySearchTree {
+public class MyBinarySearchTree<K extends Comparable<K>> {
+
+    private MyBinaryNode<K> root;
+
+    public void add(K key) {
+        this.root = this.addRecursively(root, key);
+    }
+
+    private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key){
+        if(current == null){
+            return new MyBinaryNode<>(key);
+        }
+        int compareResult = key.compareTo(current.key);
+        if (compareResult == 0)
+            return current;
+        if (compareResult < 0)
+            current.left = addRecursively(current.left, key);
+        else
+            current.right = addRecursively(current.right, key);
+        return current;
+    }
+
+    public int getSize() {
+        return this.getSizeRecursively(root);
+    }
+
+    private int getSizeRecursively(MyBinaryNode<K> root) {
+        return root == null ? 0 : 1 + this.getSizeRecursively(root.left) +
+                                      this.getSizeRecursively(root.right);
+    }
 }
--- src/test/java/com/bridgelabz/bst/MyBinarySearchTreeTest.java
+++ src/test/java/com/bridgelabz/bst/MyBinarySearchTreeTest.java
@@ -1,4 +1,19 @@
 package com.bridgelabz.bst;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MyBinarySearchTreeTest {
+
+    @Test
+    public void t(){
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+        myBinarySearchTree.add(56);
+        myBinarySearchTree.add(30);
+        myBinarySearchTree.add(70);
+        int size = myBinarySearchTree.getSize();
+        Assert.assertEquals(3,size);
+    }
+
 }
